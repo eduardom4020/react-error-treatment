0c959c7fc5a8c9d3a0bbdddaf7f051bdc33562fb
Eduardo Braga <eduardo.braga@delfosim.com>
2019-04-25 23:35:11 -0300

    We have treatered the error of the component in render command

--- src/components/WrongComponent.js
+++ src/components/WrongComponent.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 
 const thisWill = null;
 
-const WrongComponent = props => (
-    <span>{thisWill.causeAnError}</span>
-);
+const WrongComponent = props => {
+    try {
+        return(
+            <span>{thisWill.causeAnError}</span>
+        );
+    } catch (err) {
+        return (
+            <Fragment>
+                <h1>ERROR!</h1>
+                <span>{err.toString()}</span>
+            </Fragment>
+        );
+    }
+};
 
 export default WrongComponent;
